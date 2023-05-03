b343e87f777a90119a8651521cbc2446bf9e6fe3
Carlos RM <riosmonjecarlos@gmail.com>
2023-05-03 17:49:25 +0200

    2D

---
+++ non-uniform-fdtd/fdtd2d.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+eps0 = 1.0
+mu0 = 1.0
+c0 = 1/np.sqrt(eps0*mu0)
+
+class FDTD_Maxwell_2D():
+    def __init__(self, Lx=10, Ly=10, CFL=1.0, Nx=101, Ny=101, boundaryConditions=["PEC", "PEC", "PEC", "PEC"]):
+        self.x = np.linspace(0, Lx, num=Nx)
+        self.xDual = (self.x[1:] + self.x[:-1])/2
+        self.y = np.linspace(0, Ly, num=Ny)
+        self.yDual = (self.y[1:] + self.y[:-1])/2
+
+        self.dx = self.x[1] - self.x[0]
+        self.dy = self.y[1] - self.y[0]
+        self.dt = CFL * min(self.dx, self.dy) / c0
+
+        # Modo TE
+        self.Ex = np.zeros((Nx, Ny))
+        self.Ey = np.zeros((Nx, Ny))
+        self.Hz = np.zeros((Nx-1, Ny-1))
+
+        self.boundaryConditions = boundaryConditions
+    
+    def step(self):
+        Ex = self.Ex
+        Ey = self.Ey
+        Hz = self.Hz
+
+        cEx = self.dt / self.dy / eps0
+        cEy = -self.dt / self.dx / eps0
+        cHz = -self.dt / mu0
+
+        bcLx = self.boundaryConditions[0]
+        bcLy = self.boundaryConditions[1]
+        bcRx = self.boundaryConditions[2]
+        bcRy = self.boundaryConditions[3]
+       
+        # Actualizamos campos eléctricos
+        Ex[1:-1,1:-1] = Ex[1:-1,1:-1] + cEx * (Hz[:,1:] - Hz[:,:-1])
+        Ey[1:-1,1:-1] = Ey[1:-1,1:-1] + cEy * (Hz[1:,:] - Hz[:-1,:])
+
+        # Lado izquierdo
+        if bcLx == "PEC":
+            Ex[0,:] = 0.0
+            Ey[0,:] = 0.0     
+        if bcLy == "PEC":
+            Ex[:,0] = 0.0
+            Ey[:,0] = 0.0                          
+        # elif bcLx == "PMC":
+        #     Ex[0,:] = e[0,:] - 2* self.dt/self.dx/eps0*h[0,:]                  # PMC
+        # elif bcL == "Periodic":
+        #     e[0] =  (-self.dt / self.dx / eps0) * (h[0] - h[-1]) + e[0] # Periodica
+        # elif bcL == "Mur":
+        #     e[0] = eMur + (c0*self.dt-self.dx)/(c0*self.dt+self.dx)*(e[1]-e[0]) # Mur
+        else:
+            raise ValueError("Invalid boundary conditions on the left side")
+
+        # Lado derecho
+        if bcRx == "PEC":
+            Ex[-1,:] = 0.0
+            Ey[-1,:] = 0.0     
+        if bcRy == "PEC":
+            Ex[:,-1] = 0.0
+            Ey[:,-1] = 0.0
+        else:       
+            raise ValueError("Invalid boundary conditions on the left side")
+
+        Hz[:] = Hz[:] + cHz * (1/self.dx * (Ey[1:,:] - Ey[:-1,:]) - 1/self.dy * (Ex[:,1:] - Ex[:,:-1]))
---
+++ non-uniform-fdtd/test_fdtd_2D.py
@@ -0,0 +1,38 @@
+import pytest
+import numpy as np
+import matplotlib.pyplot as plt
+import fdtd2d
+
+def test_pec_box():
+    fd = fdtd2d.FDTD_Maxwell_2D(Lx=20,Ly=20,CFL=0.99,Nx=201,Ny=201,
+                                boundaryConditions=["PEC", "PEC", "PEC", "PEC"])
+            
+    x0 = 3.0; y0 = 3.0 ; s0 = 0.75
+    initialExfield = np.exp(-((fd.x - x0)**2 + (fd.y - y0)**2) / (2*s0**2))
+    initialEyfield = np.exp(-((fd.x - x0)**2 + (fd.y - y0)**2) / (2*s0**2))
+    initialHzfield = np.zeros(fd.Hz.shape)
+    fd.Ex[:] = initialExfield[:]
+    fd.Ey[:] = initialEyfield[:]
+    fd.Hz[:] = initialHzfield[:]
+    plt.figure()
+    fd.step()
+    plt.plot(fd.x, fd.y, fd.Ex, '.', label='Ex')
+    plt.plot(fd.y, fd.y, fd.Ey, '.', label='Ex')
+    plt.plot(fd.xDual, fd.yDual, fd.Hz, '.', label='Hz')
+    plt.ylim(-1.1, 1.1)
+    plt.xlim(fd.x[0], fd.x[-1])
+    plt.grid()
+    plt.figure()
+    for _ in np.arange(0, 20, fd.dt):
+        fd.step()
+        plt.plot(fd.x, fd.e, '.')
+        plt.plot(fd.xDual, fd.h, '.')
+        plt.ylim(-1.1, 1.1)
+        plt.xlim(fd.x[0], fd.x[-1])
+        plt.grid()
+        plt.pause(0.01)
+        plt.cla()
+    R = np.corrcoef(initialEfield, fd.e)
+    
+    assert(R[0,1] >= 0.999999)
+
