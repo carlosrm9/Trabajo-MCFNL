a6ffe36d688dc8c3fcc816bf702b8c732716e0fa
Carlos RM <riosmonjecarlos@gmail.com>
2023-05-04 13:09:24 +0200

    2D

--- non-uniform-fdtd/test_fdtd_2D.py
+++ non-uniform-fdtd/test_fdtd_2D.py
@@ -9,23 +9,18 @@ def test_pec_box():
                                 boundaryConditions=["PEC", "PEC", "PEC", "PEC"])
     X, Y = np.meshgrid(fd.x, fd.y)
     XDual, YDual = np.meshgrid(fd.xDual, fd.yDual)
-    x0 = 3.0; y0 = 3.0 ; s0 = 0.75
-    initialExfield = np.exp(-((X - x0)**2 + (Y - y0)**2) / (2*s0**2))
+    x0 = 10.0; y0 = 10.0 ; s0 = 0.75
+    initialExfield = np.zeros(fd.Ex.shape)
     initialEyfield = np.zeros(fd.Ey.shape)
-    initialHzfield = np.zeros(fd.Hz.shape)
+    initialHzfield = np.exp(-((XDual - x0)**2 + (YDual - y0)**2) / (2*s0**2))
     fd.Ex[:] = initialExfield[:]
     fd.Ey[:] = initialEyfield[:]
     fd.Hz[:] = initialHzfield[:]
     fig = plt.figure()
     for _ in np.arange(0, 20, fd.dt):
         fd.step()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot_surface(X, Y, fd.Ex)
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
-        ax.set_zlabel('Ex')
-        plt.show()
+        plt.contourf(XDual, YDual, fd.Hz)
         plt.grid()
-        plt.pause(0.01)
+        plt.pause(0.001)
         plt.cla()
 
