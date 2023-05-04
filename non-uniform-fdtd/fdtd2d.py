cca630a9fd77a46bbf6d5b2c2a3c5c0a6037f799
Seigel00 <danielpmares2000@gmail.com>
2023-05-04 13:45:22 +0200

    Daniel

--- non-uniform-fdtd/fdtd2d.py
+++ non-uniform-fdtd/fdtd2d.py
@@ -14,58 +14,50 @@ class FDTD_Maxwell_2D():
 
         self.dx = self.x[1] - self.x[0]
         self.dy = self.y[1] - self.y[0]
-        self.dt = CFL * min(self.dx, self.dy) / c0
+        self.dt = CFL * min(self.dx, self.dy) / c0 /np.sqrt(2)
 
         # Modo TE
-        self.Ex = np.zeros((Nx, Ny))
-        self.Ey = np.zeros((Nx, Ny))
+        self.Ex = np.zeros((Nx-1, Ny))
+        self.Ey = np.zeros((Nx, Ny-1))
         self.Hz = np.zeros((Nx-1, Ny-1))
 
         self.boundaryConditions = boundaryConditions
-    
+        self.cEx = self.dt / self.dy / eps0 
+        self.cEy = -self.dt / self.dx / eps0 
+        self.cHz = -self.dt / mu0
+
     def step(self):
         Ex = self.Ex
         Ey = self.Ey
         Hz = self.Hz
-
-        cEx = self.dt / self.dy / eps0 / 0.5
-        cEy = -self.dt / self.dx / eps0 / 0.5
-        cHz = -self.dt / mu0 / 0.5
-
-        bcLx = self.boundaryConditions[0]
-        bcLy = self.boundaryConditions[1]
-        bcRx = self.boundaryConditions[2]
-        bcRy = self.boundaryConditions[3]
+        cEx = self.cEx
+        cEy = self.cEy
+        cHz = self.cHz
+        bcL = self.boundaryConditions[0]
+        bcD = self.boundaryConditions[1]
+        bcR = self.boundaryConditions[2]
+        bcU = self.boundaryConditions[3]
        
         # Actualizamos campos eléctricos
-        Ex[1:-1,1:-1] = Ex[1:-1,1:-1] + cEx * (Hz[1:,1:] + Hz[:-1,1:] - Hz[1:,:-1] - Hz[:-1,:-1])
-        Ey[1:-1,1:-1] = Ey[1:-1,1:-1] + cEy * (Hz[1:,1:] + Hz[1:,:-1] - Hz[:-1,1:] - Hz[:-1,:-1])
+        Ex[:,1:-1] = Ex[:,1:-1] + cEx * (Hz[:,1:] - Hz[:,:-1]) 
+        Ey[1:-1,:] = Ey[1:-1,:] + cEy * (Hz[1:,:] - Hz[:-1,:]) 
 
         # Lado izquierdo
-        if bcLx == "PEC":
-            Ex[0,:] = 0.0
-            Ey[0,:] = 0.0     
-        if bcLy == "PEC":
+        if bcL == "PEC":
+            Ey[0,:] = 0.0
+            # Condición Ex
+                 
+        if bcD == "PEC":
             Ex[:,0] = 0.0
-            Ey[:,0] = 0.0                          
-        # elif bcLx == "PMC":
-        #     Ex[0,:] = e[0,:] - 2* self.dt/self.dx/eps0*h[0,:]                  # PMC
-        # elif bcL == "Periodic":
-        #     e[0] =  (-self.dt / self.dx / eps0) * (h[0] - h[-1]) + e[0] # Periodica
-        # elif bcL == "Mur":
-        #     e[0] = eMur + (c0*self.dt-self.dx)/(c0*self.dt+self.dx)*(e[1]-e[0]) # Mur
         else:
             raise ValueError("Invalid boundary conditions on the left side")
-
         # Lado derecho
-        if bcRx == "PEC":
-            Ex[-1,:] = 0.0
+        if bcR == "PEC":
             Ey[-1,:] = 0.0     
-        if bcRy == "PEC":
+        if bcU == "PEC":
             Ex[:,-1] = 0.0
-            Ey[:,-1] = 0.0
         else:       
             raise ValueError("Invalid boundary conditions on the left side")
 
-        Hz[:,:] = Hz[:,:] + cHz * (1/self.dx * (Ey[1:,1:] + Ey[1:,:-1] - Ey[:-1,1:] - Ey[:1,:-1]) 
-                                   - 1/self.dy * (Ex[1:,1:] + Ex[:-1,1:] -Ex[1:,:-1] - Ex[:-1,:-1]))
+        Hz[:,:] = Hz[:,:] + cHz * (1/self.dx * (Ey[1:,:] - Ey[:-1,:]) 
+                                   - 1/self.dy * (Ex[:,1:] - Ex[:,:-1]))
